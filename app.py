6182ce1cfedeaf2c232d8482479b15da743f4e76
William Nazate <wnazate521@gmail.com>
2020-05-25 18:04:38 -0500

    Functions Separation

--- app.py
+++ app.py
@@ -1,49 +1,59 @@
 # Kwranking App
 
+# Función que se encarga de mostrar el menú
+def muestra_menu():
+    print("-------------Kwranking-------------")
+    print("[1] - Import keywords")
+    print("[2] - Show keywords")
+    print("[0] - Exit")
 
-def menu():
+# Función que carga el fichelo con las plaabras clave
+def carga_keywords():
+    # lista que almacena cada palabra del fichero
+    keywords_list = []
+    try:
+        with open("prueba.txt", "r") as f:
+            for linea in f:
+                lines = linea.replace("\n", "")
+                keywords_list.append(lines)
+    except FileNotFoundError:
+        print("File Not Found\n")
+    return keywords_list
+
+# Función que muestra las palabras claves contenidas en el fichero
+def muestra_keywords(keywords):
+    # contador para saber cuando se muestra 20 palabras
+    counter = 0
+    for i in keywords:
+        print(i)
+        counter += 1
+        if counter == 20:
+            counter = 0
+            input("Press enter to continue")
+
+# Función principal que ejecuta el programa
+def run():
     # lista de palabras en la variable "keywords"
-    keywords = carga_keywords()
+    keywords = []
 
     while True:
-        print("[1] - Import keywords")
-        print("[2] - Show keywords")
-        print("[0] - Exit")
+        muestra_menu()
         try:
             # lectura del valor que ingresa
             option = int(input("Select an option: "))
-            if option == 1:
-                print(keywords)
-                break
-            elif option == 2:
-                # variable para imprimir el límite de 20 palabras
-                limit = 20
-                # contador para comparar e imprimir cada 20 palabras
-                counter = 0
-                for i in keywords:
-                    print(i)
-                    counter += 1
-                    if counter == limit:
-                        input("Press enter to continue")
-                        limit += 20
-                break
-            elif option == 0:
+            if option == 0:
                 print("Finalized")
                 break
+            elif option == 1:
+                keywords = carga_keywords()
+                print("\n")
+            elif option == 2:
+                print("\n")
+                muestra_keywords(keywords)
             else:
                 print("\nError! Select a correct option!\n")
         except ValueError:
             print("\n-------Enter only numbers!-------")
 
-
-def carga_keywords():
-    # lista que almacena cada palabra del fichero
-    keywords_list = []
-    with open("prueba.txt", "r") as f:
-        for linea in f:
-            lines = linea.replace("\n", "")
-            keywords_list.append(lines)
-    return keywords_list
-
-# ejecución del programa
-menu()
+# ejecución del programas
+run()
